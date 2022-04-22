446fa45843d58251461c4d5c74c1c0331d6581d4
Hosam ELnabawy <hnabawy@moj.gov.sa>
2022-04-22 19:36:39 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'squadio_task',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
